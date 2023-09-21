e30a056bf1b1ebab8ba9a5ba94a77d3b7df8a4d0
natsuki-kuroyanagi <140677506+natsuki-kuroyanagi@users.noreply.github.com>
2023-09-21 23:46:56 +0900

    Add files via upload

--- src/main/java/org/example/StreamQuizz.java
+++ src/main/java/org/example/StreamQuizz.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 public class StreamQuizz {
 
     public static void main(String[] args) {
-        List<String> names = List.of("Garmany", "Switzerland", "Italy", "Croatia", "Belgium");
+        List<String> names = List.of("Germany", "Switzerland", "Italy", "Croatia", "Belgium");
 
         List<String> sortedResult = names.stream().sorted().toList();
 
         long count = names.stream().filter(name -> name.startsWith("z")).count();
 
-        boolean hasCountryName = names.stream().anyMatch(name -> name.equals("Itary"));
+        boolean hasCountryName = names.stream().anyMatch(name -> name.equals("Italy"));
 
         names.stream().map(name -> name.toUpperCase()).collect(Collectors.toList());
         System.out.println(sortedResult);
