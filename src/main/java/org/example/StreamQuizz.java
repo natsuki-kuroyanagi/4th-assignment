06861f13e1d7f0078b34cd2212f784c7ae143678
natsuki-kuroyanagi <140677506+natsuki-kuroyanagi@users.noreply.github.com>
2023-09-21 23:06:34 +0900

    国名の頭文字を大文字に、hascountrynameをキャメルケースに変更
    国名の頭文字を大文字に、hascountrynameをキャメルケースに変更しました。

--- src/main/java/org/example/StreamQuizz.java
+++ src/main/java/org/example/StreamQuizz.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 public class StreamQuizz {
 
     public static void main(String[] args) {
-        List<String> names = List.of("garmany", "switzerland", "italy", "croatia", "belgium");
+        List<String> names = List.of("Garmany", "Switzerland", "Italy", "Croatia", "Belgium");
 
         List<String> sortedResult = names.stream().sorted().toList();
 
         long count = names.stream().filter(name -> name.startsWith("g")).count();
 
-        boolean hascountryname = names.stream().anyMatch(name -> name.equals("itary"));
+        boolean hasCountryname = names.stream().anyMatch(name -> name.equals("itary"));
 
         names.stream().map(name -> name.toUpperCase()).collect(Collectors.toList());
         System.out.println(sortedResult);
