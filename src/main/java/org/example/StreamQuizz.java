c9183295d1b5f3111d6761d887f8d7373095d308
natsuki kuroyanagi <h.t.spece6725@gmail.com>
2023-09-21 19:54:29 +0900

    第4回課題(変数名変更)

--- src/main/java/org/example/StreamQuizz.java
+++ src/main/java/org/example/StreamQuizz.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 public class StreamQuizz {
 
     public static void main(String[] args) {
-        List<String> names = List.of("aaaac", "aaaae", "aaaaa", "aaaab", "aaaad");
+        List<String> names = List.of("garmany", "switzerland", "italy", "croatia", "belgium");
 
         List<String> sortedResult = names.stream().sorted().toList();
 
-        long count = names.stream().filter(name -> name.startsWith("b")).count();
+        long count = names.stream().filter(name -> name.startsWith("g")).count();
 
-        boolean b = names.stream().anyMatch(name -> name.equals("aaaab"));
+        boolean hascountryname = names.stream().anyMatch(name -> name.equals("itary"));
 
         names.stream().map(name -> name.toUpperCase()).collect(Collectors.toList());
         System.out.println(sortedResult);
